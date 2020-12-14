e81559b7c053eb68a9d8d3c213520f6e7adc4e81
Dhruv-Vanjari <vanjaridhruv@gmail.com>
2020-12-14 14:15:22 +0530

    added horizontal collision detection

---
+++ collision.cpp
@@ -0,0 +1,44 @@
+#include"./collision.h"
+#include"./pixel.h"
+#include"./blockdata.h"
+
+// return true if cant move vertically
+bool Collision::vertical(int dir, BlockData d, Position p)
+{
+    for(int i = 0; i < BLOCK_WIDTH; i++) {
+
+        if(d.eh[(dir == 1) ? 1 : 0][i] == -1)
+            continue;
+
+        int y = d.ev[(dir == 1)? 1 : 0][i] + p.y;
+        int x = (i * Pixel::pixel) + p.x;
+
+        for(int pw = 0; pw < Pixel::pixel; pw++)
+            if(mvwinch(p.win, y + dir, x + pw) != 32)
+                return true;
+    }
+
+    return false;
+}
+
+// return true if cant move horizontal
+bool Collision::horizontal(int dir, BlockData d, Position p)
+{
+    int offset = Pixel::pixel;
+    if(dir == DIR_LEFT)
+        offset = -1;
+
+    for(int j = 0; j < BLOCK_HEIGHT; j++) {
+
+        if(d.eh[(dir == 1) ? 1 : 0][j] == -1)
+            continue;
+
+        int y = j + p.y;
+        int x = (Pixel::pixel * d.eh[(dir == 1)? 1 : 0][j]) + p.x;
+
+        if(mvwinch(p.win, y, x + offset) != 32)
+            return true;
+    }
+
+    return false;
+}
---
+++ collision.h
@@ -0,0 +1,19 @@
+#ifndef __COLLISION_H
+#define __COLLISION_H
+
+#include"blockdata.h"
+#include"position.h"
+
+#define DIR_UP    -1
+#define DIR_DOWN   1
+#define DIR_LEFT  -1
+#define DIR_RIGHT  1
+
+class Collision
+{
+public:
+    bool vertical(int dir, BlockData d, Position p);
+    bool horizontal(int dir, BlockData d, Position p);
+};
+
+#endif
