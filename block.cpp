e68b18b1805443577e48a7a3b3b2696d81339679
Dhruv-Vanjari <vanjaridhruv@gmail.com>
2020-12-14 14:20:01 +0530

    the block class

---
+++ block.cpp
@@ -0,0 +1,54 @@
+#include"blocks.h"
+#include"pixel.h"
+#include"collision.h"
+
+Block::Block(WINDOW *win, const bool *bd, int y, int x)
+{
+    d = BlockData(bd);
+    pos.x = (x * Pixel::pixel) + 1;
+    pos.y = y + 1;
+    pos.win = win;
+}
+
+void Block::show()
+{
+    initPixel(pos.win);
+    for(int i = 0; i < BLOCK_WIDTH; i++)
+        for(int j = 0; j < BLOCK_HEIGHT; j++)
+            if(d.d[j][i] == 1)
+                drawPixel(j + pos.y, (i * Pixel::pixel) + pos.x, pos.win);
+
+    endPixel(pos.win);
+}
+
+bool Block::moveVertical(int dir) {
+    Collision collision;
+    if(collision.vertical(dir, d, pos))
+        return true;
+
+    // clear pixels where the block currently is
+    for(int i = 0; i < BLOCK_WIDTH; i++)
+        for(int j = 0; j < BLOCK_HEIGHT; j++)
+            if(d.d[j][i] == 1)
+                drawPixel(j + pos.y, (i * Pixel::pixel) + pos.x, pos.win);
+
+
+    pos.y += dir;
+    return false;
+}
+
+bool Block::moveHorizontal(int dir) {
+    Collision collision;
+    if(collision.horizontal(dir, d, pos))
+        return true;
+
+    // clear pixels where the block currently is
+    for(int i = 0; i < BLOCK_WIDTH; i++)
+        for(int j = 0; j < BLOCK_HEIGHT; j++)
+            if(d.d[j][i] == 1)
+                drawPixel(j + pos.y, (i * Pixel::pixel) + pos.x, pos.win);
+
+
+    pos.x += dir * Pixel::pixel;
+    return false;
+}
--- blocks.h
+++ blocks.h
@@ -1,11 +1,34 @@
 #ifndef __BLOCKS_H
 #define __BLOCKS_H
 
+#include"blockdata.h"
+#include"position.h"
+#include"collision.h"
+
 const bool block[][9] =
 {
     {1, 1, 0, 1, 1, 0, 0, 0, 0},
-    {1, 1, 0, 0, 1, 1, 0, 0, 0}
+    {1, 1, 0, 0, 1, 1, 0, 0, 0},
+    {0, 1, 1, 1, 1, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0, 1, 1, 1}
 
+};
+
+class Block
+{
+    BlockData d;
+    Position pos;
+    Block();
+
+    bool _canMove(int dir, WINDOW* win);
+
+    public:
+    Block(WINDOW *win, const bool *, int y, int x);
+    bool moveVertical(int dir);
+    bool moveHorizontal(int dir);
+    void show();
+
+    friend class Collision;
 };
 
 #endif
